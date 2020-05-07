61046eca55a7efa04faa5da9c086bca650773011
Logesh Dinakaran <ddlogesh@gmail.com>
2020-05-07 22:20:49 +0530

    redirect-7 changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./component/home/home.component";
 
 const routes: Routes = [
-  {path:'', component:HomeComponent, pathMatch:"full"},
-  {path:'api', redirectTo: "/api", pathMatch:"full"},
-  {path:'docs', redirectTo: "/docs", pathMatch:"full"},
-  {path:'guides', redirectTo: "/docs/installation-guide/prerequisites", pathMatch:"full"},
   {path:'samples', redirectTo: "/samples", pathMatch:"full"},
-  {path:'**', redirectTo: ""}
+  {path:'', component:HomeComponent},
+  {path:'**', redirectTo: "/"}
 ];
 
 @NgModule({
