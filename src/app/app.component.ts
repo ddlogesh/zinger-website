0f187f43e9e67a5879d98fab44a059aa66aed4f5
Logesh Dinakaran <ddlogesh@gmail.com>
2020-05-07 22:51:34 +0530

    redirect-3 changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,5 @@ export class AppComponent {
 }
 export class Constants {
   public static frameworkGithubUrl = "https://github.com/ddlogesh/zinger-framework"
-  public static apiDocsUrl = "/view/6369926/Szmb6KVo"
-  public static apiDocsUrl1 = "https://documenter.getpostman.com/view/6369926/Szmb6KVo"
+  public static apiDocsUrl = "https://documenter.getpostman.com/view/6369926/Szmb6KVo"
 }
