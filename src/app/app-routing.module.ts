64288b008475c3b2da145256fc2237e75214be97
Logesh Dinakaran <ddlogesh@gmail.com>
2020-05-08 20:01:44 +0530

    routing issue-4 fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import {CaseStudyComponent} from "./component/case-study/case-study.component";
 import {HomeComponent} from "./component/home/home.component";
 
 const routes: Routes = [
-  {path: 'case-study', component: CaseStudyComponent},
   {path: '', component: HomeComponent},
+  {path: 'case-study', component: CaseStudyComponent, runGuardsAndResolvers: 'always'},
   {path: '**', redirectTo: "/"}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
